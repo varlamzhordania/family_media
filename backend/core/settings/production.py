f54db3238731487af774d0b4cf10adc362e95879
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-09-06 18:40:14 +0400

    Update production.py

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -39,7 +39,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", 'Family Media')
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
