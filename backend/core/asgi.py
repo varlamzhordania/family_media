1ff1d40a32e89d9d7b56ce7145cb90aa6d0b6bb3
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-09-06 16:57:17 +0400

    Update asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,20 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack, BaseMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-
 from core.middleware import JwtAuthMiddlewareStack
 from core.settings.settings import DEBUG
 
 from chat.routing import urlpatterns as chat_patterns
 from accounts.routing import urlpatterns as account_patterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 if DEBUG:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
@@ -30,6 +29,7 @@ else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
     print("Manage: Django loaded up in setting mode : Production")
 
+django.setup()
 django_asgi_app = get_asgi_application()
 
 urlpatterns = []
