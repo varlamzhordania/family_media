4c9e262b33bda4ce65ee7c6476391decad46ab89
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-09-07 01:51:09 +0400

    nc

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -30,7 +30,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.getenv("REDIS_HOST"), 6379)],
+            "hosts": [os.getenv("REDIS_HOST")],
         },
     },
 }
