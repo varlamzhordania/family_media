c5f72eea19d32d1516a0177296489ec4d5727dc5
Varlam Zhordania <zhordaniavarlam@gmail.com>
2025-07-15 02:48:06 +0400

    Update index.js

--- frontend/src/conf/index.js
+++ frontend/src/conf/index.js
@@ -1,12 +1,13 @@
 export const DEBUG = true
 
-export const SERVER_DOMAIN = '127.0.0.1:8000'
+export const API_SERVER_DOMAIN = '127.0.0.1:8000'
+export const WS_SERVER_DOMAIN = '127.0.0.1:8000'
 
 export const CLIENT_ID = '1xLf2JHl9N5fGk6p409HAqHzAHcyGwiqXVQh7XE5'
 export const CLIENT_SECRET = 'S8ZKQgCE4o3qmOslqyRfgjJLg1djzOyZtF4xVXiszNI6l9JKbf25PlG7ABPKCZzcAHPUHgc0dOmTncZ6hssiixdcdy9iKZ94yvtdiFGUBQRJHgro51eae47KC6nn8qAr'
 
-export const API_BASE_URL = DEBUG ? `http://${SERVER_DOMAIN}` : `https://${SERVER_DOMAIN}`
-export const SOCKET_BASE_URL = DEBUG ? `ws://${SERVER_DOMAIN}` : `wss://${SERVER_DOMAIN}`
+export const API_BASE_URL = DEBUG ? `http://${API_SERVER_DOMAIN}` : `https://${API_SERVER_DOMAIN}`
+export const SOCKET_BASE_URL = DEBUG ? `ws://${WS_SERVER_DOMAIN}` : `wss://${WS_SERVER_DOMAIN}`
 
 
 export const END_POINTS = {
